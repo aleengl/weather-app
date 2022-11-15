df5080887fd2bdaa74e04114e0346b971b46e58f
Alexander Engl <alexanderengl73@gmail.com>
2022-11-15 20:34:32 +0100

    add custom geolocation hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import { useState, useEffect, useCallback } from "react";
 import GlobalStyles from "./components/styles/Global";
 import AppContainer from "./components/styles/AppContainer.styled";
 import Grid from "./components/styles/Grid.styled";
 import CurrentLocation from "./components/currentLocation/CurrentLocation";
 import Measurement from "./components/measurement/Measurement";
 import Map from "./components/map/Map";
-import useHttp from "./components/hooks/use-http";
 import LoadingSpinner from "./components/loadingSpinner/LoadingSpinner";
 import ErrorModal from "./components/modal/ErrorModal";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import NotFound from "./components/notFound/NotFound";
-import {
-  API_URL_LOCATION,
-  API_KEY_WEATHER,
-  API_URL_FORECAST,
-} from "./constants";
+import useGeolocation from "./components/hooks/use-geolocation";
 
 const filterWeatherData = (weatherData, isPlotted = true) => {
   if (isPlotted) {
@@ -42,84 +36,8 @@ const filterWeatherData = (weatherData, isPlotted = true) => {
 };
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [message, setMessage] = useState("");
-
-  const history = useHistory();
-
-  const {
-    errorMessage,
-    sendRequest: fetchData,
-    position,
-    setPosition,
-    weatherData,
-  } = useHttp();
-
-  const success = useCallback(
-    (position) => {
-      const { latitude: lat, longitude: lon } = position.coords;
-      setMessage(
-        "Could get your position! Weather data will be available soon..."
-      );
-      setTimeout(() => {
-        setPosition({
-          lat: lat,
-          lon: lon,
-        });
-        setIsLoading(false);
-        history.push("/home");
-      }, 5000);
-    },
-    [history, setPosition]
-  );
-
-  const error = useCallback(() => {
-    setMessage(
-      "Could not get your position! Bolzano is set as the default location..."
-    );
-    setTimeout(() => {
-      fetchData(
-        `${API_URL_LOCATION}Bolzano&limit=1&appid=${API_KEY_WEATHER}`,
-        "coordinates"
-      );
-      console.log("Geolocation denied!");
-      setIsLoading(false);
-      history.push("/home");
-    }, 5000);
-  }, [history, fetchData]);
-
-  useEffect(() => {
-    // test if geolocation is available
-    if ("geolocation" in navigator) {
-      // geolocation available
-      navigator.geolocation.getCurrentPosition(success, error, {
-        enableHighAccuracy: true,
-      });
-    } else {
-      // geolocation not available
-      setMessage(
-        "The browser is not able to get your position! Bolzano is set as the default location..."
-      );
-      setTimeout(() => {
-        fetchData(
-          `${API_URL_LOCATION}Bolzano&limit=1&appid=${API_KEY_WEATHER}`,
-          "coordinates"
-        );
-        console.log("Geolocation not available!");
-        setIsLoading(false);
-        history.push("/home");
-      }, 5000);
-    }
-  }, [error, success, history, fetchData]);
-
-  useEffect(() => {
-    if (position.lat && position.lon) {
-      fetchData(
-        `${API_URL_FORECAST}${position.lat}&lon=${position.lon}&cnt=5&appid=${API_KEY_WEATHER}&units=metric`,
-        "weather"
-      );
-    }
-  }, [position, fetchData]);
+  const { isLoading, message, errorMessage, weatherData, position } =
+    useGeolocation();
 
   const plotWeatherData = filterWeatherData(weatherData);
   const forecastWeatherData = filterWeatherData(weatherData, false);
---
+++ src/components/hooks/use-geolocation.js
@@ -0,0 +1,99 @@
+import { useState, useEffect, useCallback } from "react";
+import {
+  API_URL_LOCATION,
+  API_KEY_WEATHER,
+  API_URL_FORECAST,
+} from "../../constants";
+import { useHistory } from "react-router-dom";
+import useHttp from "./use-http";
+
+const useGeolocation = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const history = useHistory();
+
+  const {
+    errorMessage,
+    sendRequest: fetchData,
+    position,
+    setPosition,
+    weatherData,
+  } = useHttp();
+
+  const success = useCallback(
+    (position) => {
+      const { latitude: lat, longitude: lon } = position.coords;
+      setMessage(
+        "Could get your position! Weather data will be available soon..."
+      );
+      setTimeout(() => {
+        setPosition({
+          lat: lat,
+          lon: lon,
+        });
+        setIsLoading(false);
+        history.push("/home");
+      }, 5000);
+    },
+    [history, setPosition]
+  );
+
+  const error = useCallback(() => {
+    setMessage(
+      "Could not get your position! Bolzano is set as the default location..."
+    );
+    setTimeout(() => {
+      fetchData(
+        `${API_URL_LOCATION}Bolzano&limit=1&appid=${API_KEY_WEATHER}`,
+        "coordinates"
+      );
+      console.log("Geolocation denied!");
+      setIsLoading(false);
+      history.push("/home");
+    }, 5000);
+  }, [history, fetchData]);
+
+  useEffect(() => {
+    // test if geolocation is available
+    if ("geolocation" in navigator) {
+      // geolocation available
+      navigator.geolocation.getCurrentPosition(success, error, {
+        enableHighAccuracy: true,
+      });
+    } else {
+      // geolocation not available
+      setMessage(
+        "The browser is not able to get your position! Bolzano is set as the default location..."
+      );
+      setTimeout(() => {
+        fetchData(
+          `${API_URL_LOCATION}Bolzano&limit=1&appid=${API_KEY_WEATHER}`,
+          "coordinates"
+        );
+        console.log("Geolocation not available!");
+        setIsLoading(false);
+        history.push("/home");
+      }, 5000);
+    }
+  }, [error, success, history, fetchData]);
+
+  useEffect(() => {
+    if (position.lat && position.lon) {
+      fetchData(
+        `${API_URL_FORECAST}${position.lat}&lon=${position.lon}&cnt=5&appid=${API_KEY_WEATHER}&units=metric`,
+        "weather"
+      );
+    }
+  }, [position, fetchData]);
+
+  return {
+    isLoading,
+    message,
+    errorMessage,
+    weatherData,
+    position,
+  };
+};
+
+export default useGeolocation;
